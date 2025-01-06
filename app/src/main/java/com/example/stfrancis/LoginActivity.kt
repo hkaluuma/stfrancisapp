3f2de7d8959a0ac5f83611a54226646bd280f15a
Hillary Kaluuma <hkaluuma@idi.co.ug>
2025-01-06 22:07:26 +0300

    first java login

--- app/src/main/java/com/example/stfrancis/LoginActivity.kt
+++ app/src/main/java/com/example/stfrancis/LoginActivity.kt
@@ -1,16 +1,29 @@
 package com.example.stfrancis
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.stfrancis.databinding.ActivityLoginBinding
+import com.example.stfrancis.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+    //added variables
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        //new codes here
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            Toast.makeText(this, "Login to home page", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
