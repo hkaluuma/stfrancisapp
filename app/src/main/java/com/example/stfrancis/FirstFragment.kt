2f7f7acc230e4fbc4799fdc79135a04205f5acba
Hillary Kaluuma <hkaluuma@idi.co.ug>
2025-01-23 20:55:18 +0300

    introduced fragments in maninactivity

--- app/src/main/java/com/example/stfrancis/FirstFragment.kt
+++ app/src/main/java/com/example/stfrancis/FirstFragment.kt
@@ -9,35 +9,7 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.stfrancis.databinding.FragmentFirstBinding
 
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(R.layout.fragment_first) {
 
-    private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-            Toast.makeText(activity, "This is a toast message", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
---
+++ app/src/main/java/com/example/stfrancis/MainFragmentView.kt
@@ -0,0 +1,36 @@
+package com.example.stfrancis
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class MainFragmentView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fragment_view)
+
+        replaceFragment(FragmentOne())
+
+
+//        binding.fragment1Btn.setOnClickListener {
+//            replaceFragment(FragmentOne())
+//        }
+//
+//        binding.fragment2Btn.setOnClickListener {
+//            replaceFragment(FragmentTwo())
+//        }
+
+    }
+
+    //new method to replace fragment
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
+    }
+
+}
