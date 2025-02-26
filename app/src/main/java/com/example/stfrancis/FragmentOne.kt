815248240e242b53c924804e1460476dada6c1db
Hillary Kaluuma <hkaluuma@idi.co.ug>
2025-02-26 09:03:36 +0300

    added back button feature

--- app/src/main/java/com/example/stfrancis/FragmentOne.kt
+++ app/src/main/java/com/example/stfrancis/FragmentOne.kt
@@ -5,5 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+
 class FragmentOne : Fragment(R.layout.fragment_one) {
+    //below is the function to react to the back button clicks
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backButton: Toolbar = view.findViewById<Toolbar>(R.id.back_button)
+        backButton.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
 }
